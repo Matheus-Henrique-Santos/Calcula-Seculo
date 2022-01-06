e71a686bed51598d4ade59a8d25c7698792c09c3
Matheus-Henrique-Santos <56160387+Matheus-Henrique-Santos@users.noreply.github.com>
2022-01-06 19:01:45 -0300

    Calcula Seculo

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf8">
+    <title>Função SeculoAno($ano)</title>
+    <meta name="author" content="">
+    <meta name="description" content="">
+    <meta name="viewport" content="width-device-width, initial-scale=1">
+</head>
+
+<body>
+    <p>Formulario para incrição de competidores</p>
+    <form action="script.php" method="get">
+        <p>Ano: <input type="text" name="ano" /></p>  
+        <!-- <p>Your age: <input type="text" name="idade" /></p> -->
+        <p><input type="submit" value="Envie o ano para saber o seculo"/></p>  
+    </form>
+
+</body>
+
+</html>
---
+++ script.php
@@ -0,0 +1,134 @@
+<?php
+
+
+$ano = $_GET['ano'];
+
+// print_r($ano);
+
+if ($ano >= 1 && $ano <=100){
+    
+    echo "O ano " .$ano . " correspode ao seculo 1";
+    
+}
+if ($ano >= 101 && $ano <=200){
+  
+    echo "O ano " .$ano . " correspode ao seculo 1";
+
+}
+
+if ($ano >= 201 && $ano <=300){
+
+    echo "O ano " .$ano . " correspode ao seculo 2";
+    
+}
+
+if ($ano >= 301 && $ano <=400){
+    
+    echo "O ano " .$ano . " correspode ao seculo 4";
+    
+}
+if ($ano >= 401 && $ano <=500){
+    
+    echo "O ano " .$ano . " correspode ao seculo 5";
+    
+}
+if ($ano >= 501 && $ano <=600){
+    
+    echo "O ano " .$ano . " correspode ao seculo 6";
+    
+}
+if ($ano >= 601 && $ano <=700){
+    
+    echo "O ano " .$ano . " correspode ao seculo 7";
+    
+}
+
+if ($ano >= 701 && $ano <=800){
+    
+    echo "O ano " .$ano . " correspode ao seculo 8";
+    
+}
+
+if ($ano >= 801 && $ano <=900){
+    
+    echo "O ano " .$ano . " correspode ao seculo 9";
+    
+}
+
+if ($ano >= 901 && $ano <=1000){
+    
+    echo "O ano " .$ano . " correspode ao seculo 10";
+    
+}
+
+if ($ano >= 1001 && $ano <=1200){
+    
+    echo "O ano " .$ano . " correspode ao seculo 11";
+    
+}
+
+if ($ano >= 1101 && $ano <=1300){
+    
+    echo "O ano " .$ano . " correspode ao seculo 12";
+    
+}
+
+if ($ano >= 1301 && $ano <=1400){
+    
+    echo "O ano " .$ano . " correspode ao seculo 13";
+    
+}
+
+if ($ano >= 1401 && $ano <=1500){
+    
+    echo "O ano " .$ano . " correspode ao seculo 14";
+    
+}
+
+if ($ano >= 1501 && $ano <=1600){
+    
+    echo "O ano " .$ano . " correspode ao seculo 15";
+    
+}
+
+if ($ano >= 1601 && $ano <=1700){
+    
+    echo "O ano " .$ano . " correspode ao seculo 16";
+    
+}
+
+if ($ano >= 1701 && $ano <=1800){
+    
+    echo "O ano " .$ano . " correspode ao seculo 17";
+    
+}
+
+if ($ano >= 1801 && $ano <=1900){
+    
+    echo "O ano " .$ano . " correspode ao seculo 18";
+    
+}
+
+if ($ano >= 1901 && $ano <=2000){
+    
+    echo "O ano " .$ano . " correspode ao seculo 19";
+    
+}
+
+if ($ano >= 2001 && $ano <=2100){
+    
+    echo "O ano " .$ano . " correspode ao seculo 20";
+
+}
+
+
+// else if($seculo >= 101 && $seculo <=200)
+//     
+//         if($seculo[$i] == 'infantil')
+//             echo "O nadador " .$ano. " compete na categoria adolescente";
+// }else{
+//     for($i =0; $i <= count($seculo); $i++){
+//         if($seculo[$i] == 'infantil')
+//             echo "O nadador " .$ano. " compete na categoria idoso";
+//     }
+// }
